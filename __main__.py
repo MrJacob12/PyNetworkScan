d0e2ff9584cfefb9d9f6ca5fb908aa216d7fe50b
MrJacob12 <anubis122003@gmail.com>
2021-03-07 17:57:28 +0100

    1

--- __main__.py
+++ __main__.py
@@ -9,6 +9,6 @@ if __name__ == "__main__":
     if major != 3 or major == 3 and minor < 6:
         print("Requires Python 3.6+\nYou are using Python %s" % (python_version))
         sys.exit(1)
-    
+
+    print('Python version:',python_version)
     import main
-    main.start
